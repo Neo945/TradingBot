18d35f042ac577ffc15619f756fe704eed17ac82
Neo945 <shreeshsrvstv@gmail.com>
2022-01-16 21:09:56 +0530

    Integrate alpaca with channels Create Orders model

--- tradingalgo/settings.py
+++ tradingalgo/settings.py
@@ -14,20 +14,25 @@ from pathlib import Path
 import os
 import alpaca_trade_api as tradeapi
 
-SEC_KEY = '' # Enter Your Secret Key Here
-PUB_KEY = '' # Enter Your Public Key Here
-BASE_URL = 'https://paper-api.alpaca.markets' # This is the base URL for paper trading
-API = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL) # For real trading, don't enter a base_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(str(BASE_DIR / 'tradingalgo/.env'))
+
+SEC_KEY = os.getenv('ALPACA_PRIVATE_KEY')
+PUB_KEY = os.getenv('ALPACA_PUBLIC_KEY')
+# BASE_URL = 'https://paper-api.alpaca.markets' # This is the base URL for paper trading
+BASE_URL = os.getenv('ALPACA_BASE_URL')
+API = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL) # For real trading, don't enter a base_url
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a^jqm%!sh&6y#*%kon$eb%w5&=^hm+ff*3=2extg$k4&te31n@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- user/models.py
+++ user/models.py
@@ -16,3 +16,20 @@ class User(models.Model):
     country = models.CharField(max_length=100,null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False)
+    symbol = models.CharField(max_length=100,null=False)
+    order_type = models.CharField(max_length=100,null=False)
+    time_in_force = models.CharField(max_length=100,null=False, default='day')
+    type = models.CharField(max_length=100,null=False, default='market')
+    quantity = models.IntegerField(null=False)
+    price = models.FloatField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # symbol=symb,
+    # qty=1,
+    # side='buy',
+    # type='market',
+    # time_in_force='gtc'
